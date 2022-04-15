04a0264000bf61b10c755ef684e4ee5d0aedd2f9
iohehe <iohehe@yeah.net>
2022-04-15 16:44:13 +0800

    recover CG

--- src/Joern.java
+++ src/Joern.java
@@ -9,8 +9,17 @@ import ast.php.functionDef.FunctionDef;
 import cfg.CFG;
 import cfg.CFGConverter;
 
+
 import udg.CFGToUDGConverter;
 import udg.useDefGraph.UseDefGraph;
+
+import ddg.DDGCreator;
+import ddg.DefUseCFG.DefUseCFG;
+import ddg.CFGAndUDGToDefUseCFG;
+import ddg.DataDependenceGraph.DDG;
+
+import cg.CG;
+import cg.CGCreator;
 
 public class Joern {
     // extract every function from the csv and recover it to ast
@@ -21,6 +30,13 @@ public class Joern {
 
     // analyze the use-define relationship for every identifier.
     static CFGToUDGConverter cfgToUDG = new CFGToUDGConverter();
+
+    // analyze data dependency flow
+    static CFGAndUDGToDefUseCFG udgAndCfgToDefUseCFG = new CFGAndUDGToDefUseCFG();
+    static DDGCreator ddgCreator = new DDGCreator();
+
+    // analyze call graph
+
 
     public static void main(String[] args) throws IOException, InvalidCSVFile {
         String nodeFileName = "demos/nodes.csv";
@@ -42,8 +58,20 @@ public class Joern {
 
             // udg
             UseDefGraph udg = cfgToUDG.convert(cfg);
-            System.out.println(udg.toString());
+            //System.out.println(udg.toString());
+
+            // ddg
+            DefUseCFG defUseCFG = udgAndCfgToDefUseCFG.convert(cfg, udg);
+            DDG ddg = ddgCreator.createForDefUseCFG(defUseCFG);
+            System.out.println("~~~~~~~~~~");
+            System.out.println(ddg.toString());
+
+            // cg
+            CGCreator.addFunctionDef(root_node);
         }
+
+        //
+        CG cg = CGCreator.createCG();
     }
 }
 
